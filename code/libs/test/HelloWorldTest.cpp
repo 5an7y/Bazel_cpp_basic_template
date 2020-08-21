32dbf982d2d3c860e2af0e94310a080b9bcd1dbe
5an7y <jose.santiago7@hotmail.com>
2020-08-20 19:47:33 -0500

    Change library to "" instead of <>

--- code/libs/test/HelloWorldTest.cpp
+++ code/libs/test/HelloWorldTest.cpp
@@ -1,11 +1,11 @@
-#include <gtest/gtest.h>
-#include "code/libs/HelloWorld.hpp"
+#include "HelloWorld.hpp"
+#include "gtest/gtest.h"
 
 TEST(HelloTest, GetGreet) {
   std::string actual = HelloWorld::returnHelloWorld();
   std::string expected = "Hello world!\n";
-  
+
   EXPECT_TRUE(true);
-  
+
   EXPECT_EQ(actual, expected);
 }
